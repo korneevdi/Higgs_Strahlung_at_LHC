478b127a620d9e3ad6594a0c380e289aa0b37c36
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-11-15 12:18:49 +0100

    Code for squared amplitudes developed

---
+++ Python_implementation/main.py
@@ -0,0 +1,14 @@
+
+import amplitudes
+
+amplitudes.loop_squared_amplitude(0)
+
+amplitudes.loop_squared_amplitude(1)
+
+amplitudes.loop_squared_amplitude(2)
+
+amplitudes.single_real_squared_amplitude(0)
+
+amplitudes.single_real_squared_amplitude(1)
+
+amplitudes.double_real_squared_amplitude()
